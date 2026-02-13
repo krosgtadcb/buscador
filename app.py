b68bfbe427270689880dab057a5c58a175d44dd2
krosgtadcb <125051052+krosgtadcb@users.noreply.github.com>
2026-02-13 08:10:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -177,4 +177,5 @@ def stats():
     return jsonify(indexador.obtener_estadisticas())
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+    app.run(debug=True, host='0.0.0.0', port=8080)
